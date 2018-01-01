a3b690a6f4bf274ca76b325f6d4c11b929c55e12
ogallagher <owengall@icloud.com>
2017-12-31 19:18:27 -0500

    Added GameController.cs

---
+++ GameController.cs
@@ -0,0 +1,27 @@
+/*
+
+Group: Owen G, Brian P, Anh V
+Date: 1 Feb 2019
+
+Course: Software Models
+
+Description:
+
+*/
+
+using System;
+
+namespace ConnectGame {
+	/// <summary>
+	/// GameController class
+	/// </summary>
+	class GameController {
+		
+		/// <summary>
+		/// Main entry point for game
+		/// </summary>
+		static void Main(string[] args) {
+			Console.WriteLine("This is ConnectGame, by Owen, Brian, and Anh.");
+		}
+	}
+}
