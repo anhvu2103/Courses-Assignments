0da6774f565ab9a9ab5ab71f360a2a3d1209511c
Owen Gallagher <owengall@icloud.com>
2019-02-24 11:29:04 -0500

    Created VS project for the GUI version

---
+++ SlideConnectWindows/SlideConnectWindows/MainWindow.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace SlideConnectWindows
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table1 = new System.Windows.Forms.TableLayoutPanel();
+            this.table2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelBoard = new System.Windows.Forms.Panel();
+            this.tableButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.tableArrows = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.labelConsole = new System.Windows.Forms.Label();
+            this.table1.SuspendLayout();
+            this.table2.SuspendLayout();
+            this.tableButtons.SuspendLayout();
+            this.tableArrows.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // table1
+            // 
+            this.table1.ColumnCount = 1;
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.table1.Controls.Add(this.table2, 0, 0);
+            this.table1.Controls.Add(this.labelConsole, 0, 1);
+            this.table1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table1.Location = new System.Drawing.Point(0, 0);
+            this.table1.Margin = new System.Windows.Forms.Padding(0);
+            this.table1.Name = "table1";
+            this.table1.RowCount = 2;
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.27907F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.72093F));
+            this.table1.Size = new System.Drawing.Size(654, 531);
+            this.table1.TabIndex = 0;
+            // 
+            // table2
+            // 
+            this.table2.ColumnCount = 2;
+            this.table2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.96053F));
+            this.table2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.03947F));
+            this.table2.Controls.Add(this.panelBoard, 0, 0);
+            this.table2.Controls.Add(this.tableButtons, 1, 0);
+            this.table2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table2.Location = new System.Drawing.Point(0, 0);
+            this.table2.Margin = new System.Windows.Forms.Padding(0);
+            this.table2.Name = "table2";
+            this.table2.RowCount = 1;
+            this.table2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.table2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.table2.Size = new System.Drawing.Size(654, 484);
+            this.table2.TabIndex = 0;
+            // 
+            // panelBoard
+            // 
+            this.panelBoard.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBoard.Location = new System.Drawing.Point(3, 3);
+            this.panelBoard.Name = "panelBoard";
+            this.panelBoard.Size = new System.Drawing.Size(503, 478);
+            this.panelBoard.TabIndex = 0;
+            // 
+            // tableButtons
+            // 
+            this.tableButtons.ColumnCount = 1;
+            this.tableButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableButtons.Controls.Add(this.buttonReset, 0, 2);
+            this.tableButtons.Controls.Add(this.buttonSave, 0, 0);
+            this.tableButtons.Controls.Add(this.buttonLoad, 0, 1);
+            this.tableButtons.Controls.Add(this.tableArrows, 0, 3);
+            this.tableButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableButtons.Location = new System.Drawing.Point(509, 0);
+            this.tableButtons.Margin = new System.Windows.Forms.Padding(0);
+            this.tableButtons.Name = "tableButtons";
+            this.tableButtons.RowCount = 4;
+            this.tableButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableButtons.Size = new System.Drawing.Size(145, 484);
+            this.tableButtons.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttonSave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonSave.Location = new System.Drawing.Point(3, 3);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(139, 115);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "save";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttonLoad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonLoad.Location = new System.Drawing.Point(3, 124);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(139, 115);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "load";
+            this.buttonLoad.UseVisualStyleBackColor = false;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttonReset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonReset.Location = new System.Drawing.Point(3, 245);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(139, 115);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "reset";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // tableArrows
+            // 
+            this.tableArrows.BackColor = System.Drawing.SystemColors.Control;
+            this.tableArrows.ColumnCount = 3;
+            this.tableArrows.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableArrows.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableArrows.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableArrows.Controls.Add(this.buttonDown, 1, 2);
+            this.tableArrows.Controls.Add(this.buttonRight, 2, 1);
+            this.tableArrows.Controls.Add(this.buttonLeft, 0, 1);
+            this.tableArrows.Controls.Add(this.buttonUp, 1, 0);
+            this.tableArrows.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableArrows.Location = new System.Drawing.Point(3, 366);
+            this.tableArrows.Name = "tableArrows";
+            this.tableArrows.RowCount = 3;
+            this.tableArrows.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableArrows.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableArrows.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableArrows.Size = new System.Drawing.Size(139, 115);
+            this.tableArrows.TabIndex = 3;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonUp.Location = new System.Drawing.Point(49, 3);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(40, 32);
+            this.buttonUp.TabIndex = 0;
+            this.buttonUp.Text = "^";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonLeft.Location = new System.Drawing.Point(3, 41);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(40, 32);
+            this.buttonLeft.TabIndex = 4;
+            this.buttonLeft.Text = "<";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRight.Location = new System.Drawing.Point(95, 41);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(41, 32);
+            this.buttonRight.TabIndex = 6;
+            this.buttonRight.Text = ">";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonDown.Location = new System.Drawing.Point(49, 79);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(40, 33);
+            this.buttonDown.TabIndex = 8;
+            this.buttonDown.Text = "v";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // labelConsole
+            // 
+            this.labelConsole.BackColor = System.Drawing.SystemColors.ControlText;
+            this.labelConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelConsole.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConsole.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelConsole.Location = new System.Drawing.Point(3, 487);
+            this.labelConsole.Margin = new System.Windows.Forms.Padding(3);
+            this.labelConsole.Name = "labelConsole";
+            this.labelConsole.Size = new System.Drawing.Size(648, 41);
+            this.labelConsole.TabIndex = 1;
+            this.labelConsole.Text = "console";
+            this.labelConsole.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 531);
+            this.Controls.Add(this.table1);
+            this.Name = "MainWindow";
+            this.Text = "Slide & Connect";
+            this.table1.ResumeLayout(false);
+            this.table2.ResumeLayout(false);
+            this.tableButtons.ResumeLayout(false);
+            this.tableArrows.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel table1;
+        private System.Windows.Forms.TableLayoutPanel table2;
+        private System.Windows.Forms.Panel panelBoard;
+        private System.Windows.Forms.TableLayoutPanel tableButtons;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.TableLayoutPanel tableArrows;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Label labelConsole;
+    }
+}
+
---
+++ SlideConnectWindows/SlideConnectWindows/MainWindow.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlideConnectWindows
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Shown += new EventHandler(window_Shown);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Saving game...";
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Loading game...";
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Resetting game...";
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Shift direction = UP";
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Shift direction = RIGHT";
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Shift direction = LEFT";
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Shift direction = DOWN";
+        }
+
+        private void window_Shown(object sender, EventArgs e)
+        {
+            labelConsole.Text = "Main window loaded";
+        }
+    }
+}
